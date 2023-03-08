718fab669bc96c9042f0834499ea58f80bcc59fe
penguin <dazhou@qq.com>
2023-03-08 21:22:26 +0800

    testss

---
+++ src/main/java/dazhou/stu.java
@@ -0,0 +1,11 @@
+package dazhou;
+
+/**
+ * @author shkstart
+ * @create 2023-03-08 21:10
+ */
+public class stu {
+    public static void main(String[] args) {
+        System.out.println("Nih1");
+    }
+}
