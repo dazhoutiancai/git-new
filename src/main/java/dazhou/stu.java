8ed240cfa51d3b7f5cef1689fc33622dafb0963c
penguin <dazhou@qq.com>
2023-03-09 22:34:37 +0800

    test heb

--- src/main/java/dazhou/stu.java
+++ src/main/java/dazhou/stu.java
@@ -8,5 +8,6 @@ public class stu {
     public static void main(String[] args) {
         System.out.println("Nih1");
         System.out.println("你是谁");
+        System.out.println("test 合并");
     }
 }
